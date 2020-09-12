026e3c21287f4c36f62818e5f3e62699969187a9
Danila Lysenko <DanyPs1@yandex.ru>
2020-09-12 16:49:14 +0300

    Code changes in lab1 and added lab report

--- Programming/Lab1/Lab1.java
+++ Programming/Lab1/Lab1.java
@@ -1,7 +1,9 @@
+import static java.lang.Math.*;
+
 public class Lab1 {
 
     public static void main(String[] args) {
-        int[] a = new int[16];;
+        int[] a = new int[16];
 
         for(int i = 17; i < 1; i--) {
             a[i] = i;
@@ -10,7 +12,7 @@ public class Lab1 {
         double[] x = new double[11];;
 
         for(int i = 0; i < x.length; i++) {
-            x[i] = -12 + Math.random() * 24;
+            x[i] = -12 + random() * 24;
         }
 
         double[][] b = new double[16][11];
@@ -19,14 +21,14 @@ public class Lab1 {
             for(int j = 0; j < 11; j++) {
                 switch (a[i]) {
                     case 6:
-                        b[i][j] = Math.exp(Math.cbrt(Math.atan(x[j] / 24)));
+                        b[i][j] = exp(cbrt(atan(x[j] / 24)));
                         break;
                     case 4: case 7: case 8: case 9:
                     case 10: case 13: case 15: case 17:
-                        b[i][j] = Math.asin(Math.cos(Math.pow(Math.sqrt(0.25 * x[j]), 4 / (Math.pow((2 / 3 - x[j]) / 2, x[j]) - 4))));
+                        b[i][j] = asin(cos(pow(sqrt(0.25 * x[j]), 4 / (pow((2 / 3 - x[j]) / 2, x[j]) - 4))));
                         break;
                     default:
-                        b[i][j] = Math.cos(Math.cos(Math.sin(Math.tan(x[j]))));
+                        b[i][j] = cos(cos(sin(tan(x[j]))));
                         break;
                 }
                 System.out.printf("%.4f%n", b[i][j]);
