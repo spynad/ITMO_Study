946b1017bcb844dbb16f6040bfda0b330ccd3baf
Danila Lysenko <DanyPs1@yandex.ru>
2020-10-04 19:52:28 +0300

    фикс лабы №1 по ОПД

--- OPD/lab0/scpt4.bash
+++ OPD/lab0/scpt4.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd lab0
 wc -m *a */*a */*/*a 1> /tmp/res1 2>/dev/null
-ls -l * */* */*/* 2>&1 | grep -v total | sort -rk5 | head -2
+(ls -l * */* */*/* | grep -v total | sort -rk5 | head -2) 2>&1
 cat -n *1 */*1 */*/*1 2>&1 | sort 
 ls -lrtu *la* */*la* */*/*la* 2>/tmp/res2
-ls -lt g* */g* */*/g* 2>&1 | egrep "^-|^l" | sort -r -k6M -k7n
-ls -l *1 */*1 */*/*1 2>/dev/null | grep "1$" | sort -r
+(ls -lt g* */g* */*/g* | egrep "^-|^l" | sort -r -k6M -k7n) 2>&1
+(ls -l *1 */*1 */*/*1 | grep "1$" | sort -r) 2>/dev/null
 cd ~
