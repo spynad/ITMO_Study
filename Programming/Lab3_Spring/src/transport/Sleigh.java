d512bdc85f8272639e16a20a66456aba469c277c
Danila Lysenko <DanyPs1@yandex.ru>
2020-12-17 18:34:00 +0300

    programming lab 4: moved to the new directory

--- Programming/Lab3_Spring/src/transport/Sleigh.java
+++ Programming/Lab3_Spring/src/transport/Sleigh.java
@@ -1,15 +1,35 @@
 package transport;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import creatures.Human;
 
 public class Sleigh {
+    private final String name;
     private ArrayList<Human> helpers = new ArrayList<>();
     private Object cargo;
 
+    Sleigh(String name) {
+        this.name = name;
+    }
+
 
     public void addHelper(Human human) {
+        if (helpers.size() < 3) {
+            helpers.add(human);
+            System.out.println(human.getName() + " now belongs to " + name);
+        } else {
+            System.out.println("Can't attach" + name + "to " + human.getName());
+        }
+    }
+
+    public void addCargo(Object object) {
+        if (cargo == null) {
+            cargo = object;
+            System.out.println("Груз " + object.toString() + " погружен в сани");
+        }
+    }
+
+    public void sendToBase() {
 
     }
 }
---
+++ Programming/untitled1/src/com/company/Resource.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Resource {
+
+}
